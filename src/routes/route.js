ad248266a24787b6237df05695761f19065801fe
Akalax <sahakash7512.com>
2021-11-18 20:20:30 +0530

    Assignment of Populate And ref

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,38 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const UserController= require("../controller/userController");
-
-const bookcontroller=require("../controller/bookcontroller");
-
-
-
-
-
-
-
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
-});
-
-router.post('/newBook', UserController.createUsers);
-router.get('/bookList', UserController.getUsersData);
-
-router.post('/newbook1',bookcontroller.bookCreation);
-router.post('/booklist1',bookcontroller.getBookData);
-router.post('/bookyear',bookcontroller.getBooksInYear);
-router.post('/inrbook',bookcontroller.getXINRBooks);
-router.post('/patiBook',bookcontroller.getParticularBooks);
-router.post('/ranBook',bookcontroller.getRandomBooks);
-//--------------------------------------------------------------------
-const authorbook=require("../controller/authorbook");
-
-router.post('/authorcreation',authorbook.authorCreation);
-router.post('/authorbookcreation',authorbook.authorBookCreation);
-router.post('/authorfind',authorbook.authorFind);
-router.post('/changeprice',authorbook.changePrice);
-router.post('/findbook',authorbook.findBook);
-
 //---------------My Author---------------------
 const myAuthorcon=require("../controller/myAuthorcon");
 
