483a2ee6f614731cd3d1928b6ea5b8c0426e40a5
Joy bhattacharya <joy.arya7381@gmail.com>
2021-11-21 00:36:40 +0530

    19 NOV niddleWare assignment

--- src/index.js
+++ src/index.js
@@ -13,3 +13,19 @@ app.use('/', route);
 app.listen(process.env.PORT || 3000, function() {
 	console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+// 19 NOV assignment - write a api and insert a middlewares and log the data everytime when any API is hit . (give date-time-Ip address-url)
+
+let dateTime = require('node-datetime');
+
+const globMid = function (req , res , next ) {
+
+    let DT = dateTime.create();
+    let format = DT.format('d/m/Y H:M:S');
+    let ipAdd = req.ip
+    let url = req.url
+    console.log(`${format} ${ipAdd} ${url}`);  
+    next()
+}
+
+app.use(globMid); 
--- src/routes/route.js
+++ src/routes/route.js
@@ -6,4 +6,8 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
+router.get("/CreateUser", function (req, res, next) {
+    res.send("middleWare assignment")
+    next();
+});
 module.exports = router;
